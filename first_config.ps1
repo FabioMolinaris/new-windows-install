4e22f7844ef953b60108f22015f1f9a8961268e6
Fabio Molinaris <fabiomoli92@gmail.com>
2023-01-26 12:35:42 +0100

    Create first_config.ps1

---
+++ first_config.ps1
@@ -0,0 +1,15 @@
+winget install -e --id Mozilla.Thunderbird
+winget install -e --id Google.Chrome
+winget install -e --id M2Team.NanaZip
+winget install -e --id VMware.WorkstationPro
+winget install vscode
+winget install -e --id Telegram.TelegramDesktop
+winget install -e --id WhatsApp.WhatsApp
+winget install -e --id Microsoft.VisualStudio.2022.Community
+winget install -e --id Microsoft.VisualStudio.2019.Community
+winget install -e --id Synology.DriveClient
+winget install -e --id Docker.DockerDesktop
+winget install -e --id Microsoft.PowerShell
+winget install -e --id TeamViewer.TeamViewer
+winget install -e --id GIMP.GIMP
+winget install -e --id Microsoft.Teams
