f878f3ffb75a4e45c095d98dea7fdaac0dcf83d2
Fabio Molinaris <fabiomoli92@gmail.com>
2025-04-16 08:59:07 +0200

    Update first_config.ps1

--- first_config.ps1
+++ first_config.ps1
@@ -1,20 +1,25 @@
 winget install -e --id Mozilla.Thunderbird
 winget install -e --id Google.Chrome
 winget install -e --id M2Team.NanaZip
-winget install -e --id VMware.WorkstationPro
-winget install vscode
 winget install -e --id Telegram.TelegramDesktop
-winget install -e --id WhatsApp.WhatsApp
-winget install -e --id Microsoft.VisualStudio.2022.Community
-winget install -e --id Microsoft.VisualStudio.2019.Community
+winget install -e --id 9NKSQGP7F2NH #WhatsApp
 winget install -e --id Synology.DriveClient
 winget install -e --id Docker.DockerDesktop
-winget install -e --id Microsoft.PowerShell
 winget install -e --id TeamViewer.TeamViewer
-winget install -e --id GIMP.GIMP
-winget install -e --id Microsoft.Teams
-winget install -e --id Microsoft.BingWallpaper
+winget install -e --id 9PNSJCLXDZ0V #GIMP
 winget install -e --id Famatech.AdvancedIPScanner
 winget install -e --id TrackerSoftware.PDF-XChangeEditor
+
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.VisualStudio.2022.Community
+winget install -e --id Microsoft.VisualStudio.2019.Community
+winget install -e --id Microsoft.SQLServerManagementStudio
+winget install -e --id Microsoft.WindowsTerminal 
+winget install -e --id Microsoft.PowerShell
+winget install -e --id Microsoft.Teams
+winget install -e --id Microsoft.BingWallpaper
+winget install -e --id 9WZDNCRFJBH4 #Microsoft Photos
+winget install -e --id 9WZDNCRFHVN5 #Windows Calculator
+winget install -e --id 9MSMLRH6LZF3 #Windows Notepad
 
 wsl --install
