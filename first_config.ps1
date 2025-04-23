241a6ae3b1c4ad8bbdd3158d5fc259b96814eba5
Fabio Molinaris <fabiomoli92@gmail.com>
2025-04-23 14:45:43 +0200

    Update first_config.ps1

--- first_config.ps1
+++ first_config.ps1
@@ -9,6 +9,7 @@ winget install -e --id TeamViewer.TeamViewer
 winget install -e --id 9PNSJCLXDZ0V #GIMP
 winget install -e --id Famatech.AdvancedIPScanner
 winget install -e --id TrackerSoftware.PDF-XChangeEditor
+winget install -e --id VB-Audio.Voicemeeter
 
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Microsoft.VisualStudio.2022.Community
@@ -17,7 +18,6 @@ winget install -e --id Microsoft.SQLServerManagementStudio
 winget install -e --id Microsoft.WindowsTerminal 
 winget install -e --id Microsoft.PowerShell
 winget install -e --id Microsoft.Teams
-winget install -e --id Microsoft.BingWallpaper
 winget install -e --id 9WZDNCRFJBH4 #Microsoft Photos
 winget install -e --id 9WZDNCRFHVN5 #Windows Calculator
 winget install -e --id 9MSMLRH6LZF3 #Windows Notepad
