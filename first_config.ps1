d42c9e9eec25fc94d2508a2c9493815f0efed1c4
Fabio Molinaris <fabiomoli92@gmail.com>
2025-04-24 09:53:16 +0200

    Update first_config.ps1

--- first_config.ps1
+++ first_config.ps1
@@ -1,25 +1,25 @@
-winget install -e --id Mozilla.Thunderbird
+winget install -e --id 9PM5VM1S3VMQ --accept-package-agreements #Mozilla.Thunderbird
 winget install -e --id Google.Chrome
-winget install -e --id M2Team.NanaZip
+winget install -e --id 9N8G7TSCL18R #M2Team.NanaZip
 winget install -e --id Telegram.TelegramDesktop
-winget install -e --id 9NKSQGP7F2NH #WhatsApp
+winget install -e --id 9NKSQGP7F2NH --accept-package-agreements #WhatsApp
 winget install -e --id Synology.DriveClient
 winget install -e --id Docker.DockerDesktop
 winget install -e --id TeamViewer.TeamViewer
-winget install -e --id 9PNSJCLXDZ0V #GIMP
+winget install -e --id 9PNSJCLXDZ0V --accept-package-agreements #GIMP
 winget install -e --id Famatech.AdvancedIPScanner
 winget install -e --id TrackerSoftware.PDF-XChangeEditor
 winget install -e --id VB-Audio.Voicemeeter
 
-winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Microsoft.VisualStudio.2022.Community
-winget install -e --id Microsoft.VisualStudio.2019.Community
+winget install -e --id XP9KHM4BK9FZ7Q --accept-package-agreements  #Microsoft.VisualStudioCode
+winget install -e --id XPDCFJDKLZJLP8 --accept-package-agreements  #Microsoft.VisualStudio.2022.Community
 winget install -e --id Microsoft.SQLServerManagementStudio
-winget install -e --id Microsoft.WindowsTerminal 
-winget install -e --id Microsoft.PowerShell
-winget install -e --id Microsoft.Teams
-winget install -e --id 9WZDNCRFJBH4 #Microsoft Photos
-winget install -e --id 9WZDNCRFHVN5 #Windows Calculator
-winget install -e --id 9MSMLRH6LZF3 #Windows Notepad
+winget install -e --id 9N0DX20HK701 --accept-package-agreements  #Microsoft.WindowsTerminal 
+winget install -e --id 9MZ1SNWT0N5D --accept-package-agreements  #Microsoft.PowerShell
+winget install -e --id XP89DCGQ3K6VLD --accept-package-agreements #Microsoft PowerToys
+winget install -e --id XP8BT8DW290MPQ --accept-package-agreements  #Microsoft.Teams
+winget install -e --id 9WZDNCRFJBH4 --accept-package-agreements #Microsoft Photos
+winget install -e --id 9WZDNCRFHVN5 --accept-package-agreements #Windows Calculator
+winget install -e --id 9MSMLRH6LZF3 --accept-package-agreements #Windows Notepad
 
 wsl --install
